fd3859e147b3d4990b191e4365c24d3b39ca1d01
yunknet <n3t.soares@gmail.com>
2026-04-02 16:15:34 -0300

    style: locked tailwind container max-width to 1024px and tweaked scale to 80% to fix horizontal stretching

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,11 @@ export default {
       center: true,
       padding: "2rem",
       screens: {
-        "2xl": "1400px",
+        "sm": "640px",
+        "md": "768px",
+        "lg": "1024px",
+        "xl": "1024px",
+        "2xl": "1024px",
       },
     },
     extend: {
